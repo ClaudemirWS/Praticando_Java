b3a31af58dc96be629fe7b3f8190b9f17e486c65
ClaudemirWS <claudemirwss@gmail.com>
2022-08-10 14:52:51 -0300

    add atvd_3 and cem_exercicios

---
+++ curso_tecnico/atvd_2/CemExercicios/src/cemexercicios/Exercicio1.java
@@ -0,0 +1,13 @@
+
+package cemexercicios;
+/**
+ * REFAZENDO OS CEM EXERCICIOS QUE FIZ EM PORTUGOL
+ * @author Claudemir Souza
+ */
+public class Exercicio1 {
+
+    public static void main(String[] args) {
+        System.out.println("Olá, mundo!");
+    }
+    
+}
