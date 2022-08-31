59ac71596e5c781bc426cc20781cddee532c80c5
ClaudemirWS <claudemirwss@gmail.com>
2022-08-31 13:31:50 -0300

    update cem_exercicios JAVA 31/08/2022

---
+++ curso_em_video/Cem_Exercicios/src/main/java/com/claudemir/cem_exercicios/Exercicio3.java
@@ -0,0 +1,24 @@
+package com.claudemir.cem_exercicios;
+
+import java.util.Scanner;
+
+/**
+ * REFAZENDO OS CEM EXERCICIOS QUE FIZ EM PORTUGOL
+ *
+ * @author Claudemir Souza
+ */
+public class Exercicio3 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String nome, salario;
+        System.out.println("===== LENDO NOME E SALÁRIO DE UM FUNCIONÁRIO =====");
+        System.out.print("Nome do funcionário: ");
+        nome = sc.nextLine();
+        System.out.print("Digite o salário do funcionário: ");
+        salario = sc.nextLine();
+        System.out.println("O salário atual do funcionário " + nome + " é R$ " + salario + ".");
+
+    }
+
+}
