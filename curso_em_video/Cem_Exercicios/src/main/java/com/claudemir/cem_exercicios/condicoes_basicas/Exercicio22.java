8ffb0f9d936026182e6cd5e5eb963ba09f877acf
ClaudemirWS <claudemirwss@gmail.com>
2022-09-06 14:55:46 -0300

    update JAVA 06/09/2022

--- curso_em_video/Cem_Exercicios/src/main/java/com/claudemir/cem_exercicios/condicoes_basicas/Exercicio22.java
+++ curso_em_video/Cem_Exercicios/src/main/java/com/claudemir/cem_exercicios/condicoes_basicas/Exercicio22.java
@@ -6,8 +6,21 @@ import java.util.Scanner;
  * @author Claudemir Souza
  */
 public class Exercicio22 {
+
     public static void main(String[] args) {
-        System.out.println("===== =====");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("===== ANOS PARA ALISTAMENTO =====");
+        int ano = 2022, nasc, idade;
+        System.out.print("Digite seu ano de nascimento: ");
+        nasc = sc.nextInt();
+        idade = ano - nasc;
+        if (idade == 18) {
+            System.out.println("Você deve se alistar neste ano.");
+        } else if (idade > 18) {
+            System.out.println("Você está " + (idade - 18) + " anos atrasado para o alistamento.");
+        } else {
+            System.out.println("Faltam " + (18 - idade) + " anos para você se alistar.");
+        }
     }
-    
+
 }
