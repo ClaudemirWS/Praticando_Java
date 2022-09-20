a84fdbe505f940f365ad5f81e44cbb6a6280bd29
ClaudemirWS <claudemirwss@gmail.com>
2022-09-20 20:41:01 -0300

    update JAVA 20/09/2022

--- curso_em_video/Cem_Exercicios/src/main/java/com/claudemir/cem_exercicios/condicoes_compostas/Exercicio31.java
+++ curso_em_video/Cem_Exercicios/src/main/java/com/claudemir/cem_exercicios/condicoes_compostas/Exercicio31.java
@@ -1,5 +1,6 @@
 package com.claudemir.cem_exercicios.condicoes_compostas;
 
+import java.util.Random;
 import java.util.Scanner;
 
 /**
@@ -8,5 +9,23 @@ import java.util.Scanner;
 public class Exercicio31 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        Random random = new Random();
+        int comp, jog;
+        System.out.println("===== PEDRA PAPEL OU TESOURA =====");
+        comp = random.nextInt(3);
+        System.out.print("0 - PEDRA | 1 - PAPEL | 2 - TESOURA: ");
+        jog = sc.nextInt();
+        if (comp == jog)
+        {
+            System.out.println("Empate!");
+        }
+        else if (comp == 0 && jog == 2 || comp == 1 && jog == 0 || comp == 2 && jog == 1)
+        {
+            System.out.println("O computador ganhou!");
+        }
+        else
+        {
+            System.out.println("O jogador ganhou!");
+        }        
     }
 }
---
+++ curso_em_video/Cem_Exercicios/src/main/java/com/claudemir/cem_exercicios/condicoes_compostas/Exercicio32.java
@@ -0,0 +1,15 @@
+package com.claudemir.cem_exercicios.condicoes_compostas;
+
+import java.util.Random;
+import java.util.Scanner;
+
+/**
+ * @author Claudemir Souza
+ */
+public class Exercicio32 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int comp, jog;
+    
+    }
+}
