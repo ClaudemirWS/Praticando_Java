8fcdec96418fff4ec1931a579e03b8efe837358e
ClaudemirWS <claudemirwss@gmail.com>
2022-09-21 07:44:09 -0300

    update JAVA 21/09/2022

--- curso_em_video/Cem_Exercicios/src/main/java/com/claudemir/cem_exercicios/condicoes_compostas/Exercicio32.java
+++ curso_em_video/Cem_Exercicios/src/main/java/com/claudemir/cem_exercicios/condicoes_compostas/Exercicio32.java
@@ -7,9 +7,20 @@ import java.util.Scanner;
  * @author Claudemir Souza
  */
 public class Exercicio32 {
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        Random random = new Random();
         int comp, jog;
-    
+        comp = random.nextInt(5);
+        System.out.println("===== ACERTE O NÚMERO DE 1 A 5 =====");
+        System.out.print("Número: ");
+        jog = sc.nextInt();
+        if (jog == comp) {
+            System.out.println("Acertou!");
+        } else {
+            System.out.println("Errou!");
+        }
+
     }
 }
