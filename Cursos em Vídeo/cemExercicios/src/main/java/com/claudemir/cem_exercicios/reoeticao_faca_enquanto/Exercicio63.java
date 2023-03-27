9a6cd0ef086eb7a628ba280c320c838daa457452
Claudemir <claudemirwss@gmail.com>
2023-03-27 19:35:49 -0300

    add exercicio63

---
+++ Cursos em Vídeo/cemExercicios/src/main/java/com/claudemir/cem_exercicios/reoeticao_faca_enquanto/Exercicio63.java
@@ -0,0 +1,11 @@
+package com.claudemir.cem_exercicios.reoeticao_faca_enquanto;
+
+/**
+ * @author Claudemir
+ */
+public class Exercicio63 {
+
+    public static void main(String[] args) {
+
+    }
+}
