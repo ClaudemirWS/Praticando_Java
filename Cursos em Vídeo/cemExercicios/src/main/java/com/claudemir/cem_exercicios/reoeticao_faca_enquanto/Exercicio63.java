604798892a433739baf3ce75f8b461d9810fe55f
Claudemir <claudemirwss@gmail.com>
2023-03-29 09:35:09 -0300

    update final Exercicio63

--- Cursos em Vídeo/cemExercicios/src/main/java/com/claudemir/cem_exercicios/reoeticao_faca_enquanto/Exercicio63.java
+++ Cursos em Vídeo/cemExercicios/src/main/java/com/claudemir/cem_exercicios/reoeticao_faca_enquanto/Exercicio63.java
@@ -1,5 +1,6 @@
 package com.claudemir.cem_exercicios.reoeticao_faca_enquanto;
 
+import java.util.Scanner;
 import javax.swing.JOptionPane;
 
 /**
@@ -12,28 +13,31 @@ public class Exercicio63 {
         int n, soma = 0, menor = 0, qtd = 0, pares = 0;
 
         do {
-            n = Integer.parseInt(JOptionPane.showInputDialog(null, "Digite um número: "));
-            //SOMA TODOS OS NÚMEROS
-            soma += n;
-            //ADICIONA UM NÚMERO A QUANTIDADE
-            qtd += 1;
-            //ARMAZENA O MENOR NÚMERO
-            if (qtd == 0) {
-                menor = n;
-            } else if (n < menor) {
-                menor = n;
+            Scanner sc = new Scanner(System.in);
+            System.out.print("Digite um número (0 para SAIR): ");
+            n = sc.nextInt();
+            if (n != 0) {
+                //SOMA TODOS OS NÚMEROS
+                soma += n;
+                //ARMAZENA O MENOR NÚMERO
+                if (qtd == 0) {
+                    menor = n;
+                } else if (n < menor) {
+                    menor = n;
+                }
+                //CONTA QUANTOS PARES EXISTEM
+                if (n % 2 == 0) {
+                    pares += 1;
+                }
+                //AUMENTA UM NÚMERO A QUANTIDADE
+                qtd += 1;
             }
-            //CONTA QUANTOS PARES EXISTEM
-            if (n % 2 == 0) {
-                pares += 1;
-            }
-            prog = JOptionPane.showInputDialog(null, "Deseja continuar? (S/N): ").strip().toUpperCase();
-        } while (!prog.equals("N"));
+        } while (n != 0);
 
-        System.out.println(qtd + " números foram cadastrados.");
+        System.out.println("\n" + qtd + " números foram cadastrados.");
         System.out.println("A SOMA entre todos foi " + soma);
         System.out.println("O MENOR número foi o " + menor);
-        System.out.println("A MÉDIA entre todos os números foi " + qtd / soma);
+        System.out.println("A MÉDIA entre todos os números foi " + (soma/qtd));
         System.out.println(pares + " números são PARES.");
     }
 }
