ccb63efc408500535fb1d2b92e452c58b8cdfaea
Claudemir <claudemirwss@gmail.com>
2023-03-30 10:36:56 -0300

    add exercicio64

---
+++ Cursos em Vídeo/cemExercicios/src/main/java/com/claudemir/cem_exercicios/repeticao_para/Exercicio64.java
@@ -0,0 +1,15 @@
+package com.claudemir.cem_exercicios.repeticao_para;
+
+/**
+ * @author Claudemir
+ */
+public class Exercicio64 {
+
+    public static void main(String[] args) {
+        int n = 45;
+        for (int i = 0; i < n; i += 5){
+            System.out.print(" " + i + " ");
+        }
+        System.out.print(" Acabou!");
+    }
+}
