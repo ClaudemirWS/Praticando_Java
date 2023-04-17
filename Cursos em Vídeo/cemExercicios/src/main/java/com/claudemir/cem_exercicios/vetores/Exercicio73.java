82db23b82b60de7316384b5c2d76c2a6d847a52e
Claudemir <claudemirwss@gmail.com>
2023-04-17 08:09:22 -0300

    add Exercicio73

---
+++ Cursos em Vídeo/cemExercicios/src/main/java/com/claudemir/cem_exercicios/vetores/Exercicio73.java
@@ -0,0 +1,17 @@
+package com.claudemir.cem_exercicios.vetores;
+
+/**
+ * @author Claudemir
+ */
+public class Exercicio73 {
+
+    public static void main(String[] args) {
+        int vetor[] = new int[10];
+        for (int i = 0; i <= 0; i++) {
+            for (int j = 9; j >= 0; j--) {
+                vetor[i] = j;
+                System.out.print("["+vetor[i]+"] ");
+            }
+        }
+    }
+}
