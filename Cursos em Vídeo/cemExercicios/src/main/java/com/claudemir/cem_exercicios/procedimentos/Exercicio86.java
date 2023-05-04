da008a10d41bb046864340217304d9d60b6b6e76
Claudemir Souza <claudemirwss@Gmail.com>
2023-05-04 15:44:41 -0300

    add Exercicio86

---
+++ Cursos em Vídeo/cemExercicios/src/main/java/com/claudemir/cem_exercicios/procedimentos/Exercicio86.java
@@ -0,0 +1,19 @@
+package com.claudemir.cem_exercicios.procedimentos;
+
+/**
+ * @author Claudemir
+ */
+public class Exercicio86 {
+
+    public static void main(String[] args) {
+        desenho();
+        System.out.print("Olá, mundo!");
+        desenho();
+        
+    }
+    
+    //PRIMEIRO EXEMPLO DE FUNÇÃO
+    private static void desenho(){
+        System.out.println("\n+-------------------+");
+    }
+}
